9263d851be391a4d8f8bd8843bd628156996d23c
chc <chc@Choel-Houi-MacBookPro.local>
2022-07-09 08:47:25 +0900

    m

--- ICT_MRI_interpolation/dataset/check_dataset.py
+++ ICT_MRI_interpolation/dataset/check_dataset.py
@@ -7,41 +7,13 @@ import pandas as pd
 
 
 
-if __name__=="__main__":
-    
-    _dirs=os.listdir("/mnt/hdd0/ICT_DATASET_ALL")
-    _dirs_eval=os.listdir("/mnt/hdd0/ICT_DATASET_EVAL")
-    _dirs_png=os.listdir("/mnt/hdd0/ICT_DATASET_PNG")
-    
-    
-    print(len(_dirs))
-    print(len(_dirs_eval))
-    print(len(_dirs_png))
-    
 
-    
-    # for i in _dirs_eval:
-    #     _dirs.remove(i)
-        
-    # print(len(_dirs))
-    
-    
-    # for trg_dir in _dirs:
-    #     shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_PNG/"+trg_dir)
-        
-    # exit()
-    
-    
-    
-    df=pd.read_excel("./labels/CHC_220420.xlsx", engine='openpyxl')
-    
-    print(df)
-    
-    
-    print(df["label"])
-    print(df["PET"])
-    print(df['nuc_t1_inServer'])
-    
+
+
+
+
+def num_of_label_check():
+    df=pd.read_excel("./labels/220420_th_with_demo.xlsx", engine='openpyxl')
     
     pos_add=[]
     neg_nc=[]
@@ -58,34 +30,52 @@ if __name__=="__main__":
     print(pos_add)
     print(pos_add.index("IDEA_052_120429_nuc_t1"))
     
+    
+    exit()
+    
     while True:
         a=int(input("type index"))
         print(pos_add[a])
     
-    exit()
-    
-    # for i in pos_add:
-    #     try:
-    #         trg_dir=i+"_resample"
-    #         shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_EVAL/"+trg_dir)
-    #         num_pos_add+=1
-    #     except:
-    #         continue
+    pass
+
+
+def aa():
+    for i in pos_add:
+        try:
+            trg_dir=i+"_resample"
+            shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_EVAL/"+trg_dir)
+            num_pos_add+=1
+        except:
+            continue
             
-    #     if num_pos_add==50:
-    #         break
+        if num_pos_add==50:
+            break
         
     
-    # for i in neg_nc:
-    #     try:
-    #         trg_dir=i+"_resample"
-    #         shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_EVAL/"+trg_dir)
-    #         num_neg_nc+=1
-    #     except:
-    #         continue
+    for i in neg_nc:
+        try:
+            trg_dir=i+"_resample"
+            shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_EVAL/"+trg_dir)
+            num_neg_nc+=1
+        except:
+            continue
         
-    #     if num_neg_nc==50:
-    #         break
+        if num_neg_nc==50:
+            break
+    
+
+
+
+
+# the code for dataset sanity check
+
+if __name__=="__main__":
+    
+    
+    
+    
+    pass
         
         
         
@@ -93,9 +83,6 @@ if __name__=="__main__":
     
     
     
-    
-    print(len(pos_add))
-    print(len(neg_nc))
     
     
     
