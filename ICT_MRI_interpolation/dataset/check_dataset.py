19053e55e05106ff4c825a7596d3d2d78ae66104
“Cheol-Ho” <hoonchcho@gmail.com>
2022-07-10 08:55:22 +0000

    make class form

--- ICT_MRI_interpolation/dataset/check_dataset.py
+++ ICT_MRI_interpolation/dataset/check_dataset.py
@@ -6,89 +6,90 @@ from turtle import pos
 import pandas as pd
 from tqdm import tqdm
 import time
+import random
 
 
 
+class Split_Dataset:
+    def __init__(self) -> None:
 
+        self.pos_ad = []
+        self.neg_ad = []
+        self.folds = []
+        
 
-
-
-
-def num_of_label_check() -> list:
+    def extract_pos_ad_neg_nc(self) -> None:
      
-    print("check the number of pet_pos_ad & pet_neg_nc")
+        print("check the number of pet_pos_ad & pet_neg_nc")
 
-    print("data loading...")
-    
-    start_time = time.time()
-    df=pd.read_excel("../labels/220420_th_with_demo.xlsx", engine='openpyxl')
-    end_time = time.time()
-    print(f"data loaded. the time spent is {end_time-start_time:.3f}s ")
-    
-    
-    pos_ad=[]
-    neg_nc=[]
-    for code, pet , label in zip(tqdm(df['nuc_t1_inServer']),df["PET"],df['label']):
-        if pet == 1 and label == "ADD":
-            pos_ad.append(code.split(".")[0])
-        elif pet == 0 and label == "NC":
-            neg_nc.append(code.split(".")[0])
-            
+        print("data loading...")
+        
+        start_time = time.time()
+        df=pd.read_excel("../labels/220420_th_with_demo.xlsx", engine='openpyxl')
+        end_time = time.time()
+        print(f"data loaded. the time spent is {end_time-start_time:.3f}s ")
+        
+        for code, pet , label in zip(tqdm(df['nuc_t1_inServer']),df["PET"],df['label']):
+            if pet == 1 and label == "ADD":
+                self.pos_ad.append(code.split(".")[0])
+            elif pet == 0 and label == "NC":
+                self.neg_nc.append(code.split(".")[0])
+                
 
-    print(f"total data number : {len(df['PET'])}")
-    print(f"num of pet pos ad : {len(pos_ad)}")
-    print(f"num of pet neg nc : {len(neg_nc)}")
-    
-    return pos_ad , neg_nc
-
-def overlap_test(folds:list) -> None:
-    #Check if there is any overlap
-    
-    for i in folds[0]:
-        assert i not in fold[1] , "fold '0' sample overlap with fold[1]"
-        assert i not in fold[2] , "fold '1' sample overlap with fold[1]"
-        assert i not in fold[4] , "fold '2' sample overlap with fold[1]"
-        assert i not in fold[3] , "fold '3' sample overlap with fold[1]"
-    
-    
+        print(f"total data number : {len(df['PET'])}")
+        print(f"num of pet pos ad : {len(self.pos_ad)}")
+        print(f"num of pet neg nc : {len(self.neg_nc)}")
 
 
+        # shuffle pos_ad , neg_nc
+        random.seed(9999)
+        random.shuffle(self.pos_ad)
+        random.shuffle(self.neg_nc)
+        
+        print("extract pos_ad , neg_nc done")
 
-def split_folds_for_cv(pos_ad:list , neg_nc:list, total_test_num:int = 130, split_num:int = 5)-> dict: 
-    
-    #divide test num into pos , neg halves
-    each_test_num = total_test_num / 2
-    
-    folds = []
-    
-    #devide test data into 5 folds
-    _start_idx = 0 
-    for _split in range(split_num):
-        _end_idx = _start_idx + each_test_num
-        _cur_pos = pos_ad[_start_idx:_end_idx]
-        _cur_neg = neg_nc[_start_idx:_end_idx]
-        folds.append({"pos_ad": _cur_pos , "neg_nc":_cur_neg})
-        _start_idx = _end_idx
-    
 
-    overlap_test(folds=folds)
+    def overlap_test(self) -> None:
+        #Check if there is any overlap
+        
+        print("overlap test start")
+        for i in tqdm(self.folds[0]["pos_ad"]):
+            assert i not in self.folds[1]["pos_ad"] , "fold '0' pos_ad sample overlap with fold[1]"
+            assert i not in self.folds[2]["pos_ad"] , "fold '0' pos_ad sample overlap with fold[2]"
+            assert i not in self.folds[3]["pos_ad"] , "fold '0' pos_ad sample overlap with fold[3]"
+            assert i not in self.folds[4]["pos_ad"] , "fold '0' pos_ad sample overlap with fold[4]"
+
+        for i in tqdm(self.folds[0]["neg_nc"]):
+            assert i not in self.folds[1]["neg_nc"] , "fold '0' neg_nc sample overlap with fold[1]"
+            assert i not in self.folds[2]["neg_nc"] , "fold '0' neg_nc sample overlap with fold[2]"
+            assert i not in self.folds[3]["neg_nc"] , "fold '0' neg_nc sample overlap with fold[3]"
+            assert i not in self.folds[4]["neg_nc"] , "fold '0' neg_nc sample overlap with fold[4]"
+
+        print("there is no overlap")
+    
+    
+    def split_folds_for_cv(self, total_test_num:int = 130, split_num:int = 5)-> None: 
+        
+        #divide test num into pos , neg halves
+        each_test_num = int(total_test_num / 2)
+        
+        #devide test data into 5 folds
+        _start_idx = 0 
+        for _split in range(split_num):
+            _end_idx = _start_idx + each_test_num
+            _cur_pos = self.pos_ad[_start_idx:_end_idx]
+            _cur_neg = self.neg_nc[_start_idx:_end_idx]
+            self.folds.append({"pos_ad": _cur_pos , "neg_nc":_cur_neg})
+            _start_idx = _end_idx
+        
+        #test for there are overlap between folds
+        overlap_test(folds=self.folds)
         
     
+    def create_folds_dataset(folds: list) -> None:
         
-     
-    
+        pass
         
-    
-        
-        
-        
-        
-    
-    
-    
-    return folds
-    
-    
     
 
 def aa():
@@ -122,7 +123,14 @@ def aa():
 # the code for dataset sanity check
 
 if __name__=="__main__":
-    num_of_label_check()
+
+    split_dataset=Split_Dataset()
+
+    split_dataset.extract_pos_ad_neg_nc()
+    split_dataset.split_folds_for_cv()
+    
+
+
     
     
     
