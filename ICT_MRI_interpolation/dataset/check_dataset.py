ad5310d446d4a181232348422cd6ef2f5f893551
chc <chc@Choel-Houi-MacBookPro.local>
2022-07-10 13:52:28 +0900

    m

--- ICT_MRI_interpolation/dataset/check_dataset.py
+++ ICT_MRI_interpolation/dataset/check_dataset.py
@@ -26,31 +26,81 @@ def num_of_label_check() -> list:
     print(f"data loaded. the time spent is {end_time-start_time:.3f}s ")
     
     
-    pos_add=[]
+    pos_ad=[]
     neg_nc=[]
     for code, pet , label in zip(tqdm(df['nuc_t1_inServer']),df["PET"],df['label']):
         if pet == 1 and label == "ADD":
-            pos_add.append(code.split(".")[0])
+            pos_ad.append(code.split(".")[0])
         elif pet == 0 and label == "NC":
             neg_nc.append(code.split(".")[0])
             
 
     print(f"total data number : {len(df['PET'])}")
-    print(f"num of pet pos ad : {len(pos_add)}")
+    print(f"num of pet pos ad : {len(pos_ad)}")
     print(f"num of pet neg nc : {len(neg_nc)}")
     
-    return pos_add , neg_nc
+    return pos_ad , neg_nc
+
+def overlap_test(folds:list) -> None:
+    #Check if there is any overlap
+    
+    for i in folds[0]:
+        assert i not in fold[1] , "fold '0' sample overlap with fold[1]"
+        assert i not in fold[2] , "fold '1' sample overlap with fold[1]"
+        assert i not in fold[4] , "fold '2' sample overlap with fold[1]"
+        assert i not in fold[3] , "fold '3' sample overlap with fold[1]"
+    
+    
+
+
+
+def split_folds_for_cv(pos_ad:list , neg_nc:list, total_test_num:int = 130, split_num:int = 5)-> dict: 
+    
+    #divide test num into pos , neg halves
+    each_test_num = total_test_num / 2
+    
+    folds = []
+    
+    #devide test data into 5 folds
+    _start_idx = 0 
+    for _split in range(split_num):
+        _end_idx = _start_idx + each_test_num
+        _cur_pos = pos_ad[_start_idx:_end_idx]
+        _cur_neg = neg_nc[_start_idx:_end_idx]
+        folds.append({"pos_ad": _cur_pos , "neg_nc":_cur_neg})
+        _start_idx = _end_idx
+    
+
+    overlap_test(folds=folds)
+        
+    
+        
+     
+    
+        
+    
+        
+        
+        
+        
+    
+    
+    
+    return folds
+    
+    
+    
 
 def aa():
-    for i in pos_add:
+    for i in pos_ad:
         try:
             trg_dir=i+"_resample"
             shutil.copytree("/mnt/hdd0/ICT_DATASET_ALL/"+trg_dir,"/mnt/hdd0/ICT_DATASET_EVAL/"+trg_dir)
-            num_pos_add+=1
+            num_pos_ad+=1
         except:
             continue
             
-        if num_pos_add==50:
+        if num_pos_ad==50:
             break
         
     
