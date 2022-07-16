e0020fa6c285a3de2926ac552af18a764fd1b24e
CHC <hoonchcho@gmail.com>
2022-07-16 11:44:34 +0000

    m

--- ICT_MRI_interpolation/train/train.py
+++ ICT_MRI_interpolation/train/train.py
@@ -166,8 +166,8 @@ if __name__ == "__main__":
     if args.load_model:
         print("load model")
         assert isinstance(args.load_epoch, int) , "input load_epoch argument"
-        load_dir = f'./checkpoint/flownet_{args.load_epoch}'
-        model.load_model(load_dir,-1)
+        load_dir = f'./checkpoint/'
+        model.load_model(load_dir,args.load_epoch)
         print("model load done")
     os.makedirs(args.log_path, exist_ok=True)
     train(model, args)
