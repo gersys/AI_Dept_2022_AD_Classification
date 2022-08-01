d28c4f693094241fbc66cba42c55cd83e494e6da
CHC <hoonchcho@gmail.com>
2022-08-01 08:14:05 +0000

    m

---
+++ ICT_MRI_interpolation/inference/eval_interpolation_linear.sh
@@ -0,0 +1,6 @@
+python inference_linear.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 1 --out ./linear_results/0/eval/x2  --gt_dir  /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_linear.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 2 --out ./linear_results/0/eval/x4  --gt_dir  /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_linear.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 3 --out ./linear_results/0/eval/x8  --gt_dir  /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_linear.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 4 --out ./linear_results/0/eval/x16 --gt_dir  /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_linear.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 5 --out ./linear_results/0/eval/x32 --gt_dir  /mnt/ICT_DATASET_FOLDS/0/eval/
+
---
+++ ICT_MRI_interpolation/inference/eval_interpolation_png.sh
@@ -0,0 +1,6 @@
+python inference_ict.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 1 --epoch 70 --model /project/Project/ICT_medical_AD_VFI/ICT_MRI_interpolation/train/checkpoints --out ./results/0/eval/x2  --gt_dir /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_ict.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 2 --epoch 70 --model /project/Project/ICT_medical_AD_VFI/ICT_MRI_interpolation/train/checkpoints --out ./results/0/eval/x4  --gt_dir /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_ict.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 3 --epoch 70 --model /project/Project/ICT_medical_AD_VFI/ICT_MRI_interpolation/train/checkpoints --out ./results/0/eval/x8  --gt_dir /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_ict.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 4 --epoch 70 --model /project/Project/ICT_medical_AD_VFI/ICT_MRI_interpolation/train/checkpoints --out ./results/0/eval/x16 --gt_dir /mnt/ICT_DATASET_FOLDS/0/eval/
+python inference_ict.py --img /mnt/ICT_DATASET_FOLDS/0/eval/ --exp 5 --epoch 70 --model /project/Project/ICT_medical_AD_VFI/ICT_MRI_interpolation/train/checkpoints --out ./results/0/eval/x32 --gt_dir /mnt/ICT_DATASET_FOLDS/0/eval/
+
